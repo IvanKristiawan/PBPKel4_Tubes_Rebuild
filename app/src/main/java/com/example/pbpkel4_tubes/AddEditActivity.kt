e78f1ba8e3239be87d063eb6eedb2c79bded3104
stillmanflobert <stillmanflobert2303@gmail.com>
2022-11-24 15:18:40 +0700

    Add 1 Library PDF

--- app/src/main/java/com/example/pbpkel4_tubes/AddEditActivity.kt
+++ app/src/main/java/com/example/pbpkel4_tubes/AddEditActivity.kt
@@ -1,21 +1,48 @@
 package com.example.pbpkel4_tubes
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.view.WindowManager
 import android.widget.*
+import androidx.annotation.RequiresApi
 import com.android.volley.AuthFailureError
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.pbpkel4_tubes.api.PaketTravelApi
+import com.example.pbpkel4_tubes.databinding.ActivityAddEditBinding
 import com.example.pbpkel4_tubes.models.PaketTravel
 import com.google.gson.Gson
+import com.itextpdf.barcodes.BarcodeQRCode
+import com.itextpdf.io.image.ImageDataFactory
+import com.itextpdf.io.source.ByteArrayOutputStream
+import com.itextpdf.kernel.colors.ColorConstants
+import com.itextpdf.kernel.geom.PageSize
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Cell
+import com.itextpdf.layout.element.Image
+import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
+import com.itextpdf.layout.property.HorizontalAlignment
+import com.itextpdf.layout.property.TextAlignment
 import org.json.JSONObject
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.nio.charset.StandardCharsets
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class AddEditActivity : AppCompatActivity() {
     private var etIdPaket: EditText? = null
@@ -28,8 +55,11 @@ class AddEditActivity : AppCompatActivity() {
     private var layoutLoading: LinearLayout? = null
     private var queue: RequestQueue? = null
 
+    private var binding: ActivityAddEditBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAddEditBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_add_edit)
 
         queue = Volley.newRequestQueue(this)
@@ -57,6 +87,8 @@ class AddEditActivity : AppCompatActivity() {
             btnSave.setOnClickListener { updatePaketTravel(id) }
         }
     }
+
+
 
     private fun getPaketTravelByid(id: String?){
         setLoading(true)
@@ -119,7 +151,7 @@ class AddEditActivity : AppCompatActivity() {
 
                 if(mahasiswa != null)
                     Toast.makeText( this@AddEditActivity, "Data berhasil ditambahkan", Toast.LENGTH_SHORT).show()
-
+                createPdf(mahasiswa.idPaket,mahasiswa.namaPaket,mahasiswa.tujuan,mahasiswa.asal,mahasiswa.harga,mahasiswa.jam, mahasiswa.durasi)
                 val returnIntent = Intent()
                 setResult(RESULT_OK, returnIntent)
                 finish()
@@ -234,5 +266,88 @@ class AddEditActivity : AppCompatActivity() {
             layoutLoading!!.visibility = View.INVISIBLE
         }
     }
+// pdf
 
+    @SuppressLint("ObsoleteSdkInt")
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Throws(
+        FileNotFoundException::class
+    )
+
+    private fun createPdf(idPaket: String, namapaket: String, tujuan: String, asal: String, harga: String, jam: String, durasi: String) {
+        val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+        val file = File(pdfPath, "pdf_travel.pdf")
+        FileOutputStream(file)
+
+        val writer = PdfWriter(file)
+        val pdfDocument = PdfDocument(writer)
+        val document = Document(pdfDocument)
+        pdfDocument.defaultPageSize = PageSize.A4
+        document.setMargins(5f,5f,5f,5f)
+        @SuppressLint("UseCompatLoadingForDrawables") val d =getDrawable(R.drawable.travel)
+
+        val bitmap = (d as BitmapDrawable?)!!.bitmap
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bitmapData = stream.toByteArray()
+        val imageData = ImageDataFactory.create(bitmapData)
+        val image = Image(imageData)
+        val namaPengguna = Paragraph("Detail Paket Penerbangan").setBold().setFontSize(24f).setTextAlignment(
+            TextAlignment.CENTER)
+        val group = Paragraph(
+            """
+                Detail Penerbangan 
+                ..................
+            """.trimIndent()
+        ).setTextAlignment(TextAlignment.CENTER).setFontSize(12f)
+
+        val width = floatArrayOf(100f, 100f)
+        val table = Table(width)
+        table.setHorizontalAlignment(HorizontalAlignment.CENTER)
+        table.addCell(Cell().add(Paragraph("Id Paket")))
+        table.addCell(Cell().add(Paragraph(idPaket)))
+        table.addCell(Cell().add(Paragraph("Nama Paket")))
+        table.addCell(Cell().add(Paragraph(namapaket)))
+        table.addCell(Cell().add(Paragraph("Tujuan")))
+        table.addCell(Cell().add(Paragraph(tujuan)))
+        table.addCell(Cell().add(Paragraph("Asal")))
+        table.addCell(Cell().add(Paragraph(asal)))
+        table.addCell(Cell().add(Paragraph("harga")))
+        table.addCell(Cell().add(Paragraph(harga)))
+        table.addCell(Cell().add(Paragraph("jam")))
+        table.addCell(Cell().add(Paragraph(jam)))
+        table.addCell(Cell().add(Paragraph("durasi")))
+        table.addCell(Cell().add(Paragraph(durasi)))
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        table.addCell(Cell().add(Paragraph("Tanggal Buat Pdf")))
+        table.addCell(Cell().add(Paragraph(LocalDate.now().format(dateTimeFormatter))))
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss a")
+        table.addCell(Cell().add(Paragraph("Pukul Pembuatan")))
+        table.addCell(Cell().add(Paragraph(LocalTime.now().format(timeFormatter))))
+
+
+        val barcodeQRCode = BarcodeQRCode(
+            """
+                $idPaket
+                $namapaket
+                $tujuan
+                $asal
+                $harga
+                $jam
+                $durasi
+                ${LocalDate.now().format(dateTimeFormatter)}
+                ${LocalTime.now().format(timeFormatter)}
+            """.trimIndent())
+        val qrCodeObject = barcodeQRCode.createFormXObject(ColorConstants.BLACK, pdfDocument)
+        val qrCodeImage = Image(qrCodeObject).setWidth(80f).setHorizontalAlignment(HorizontalAlignment.CENTER)
+
+        document.add(image)
+        document.add(namaPengguna)
+        document.add(group)
+        document.add(table)
+        document.add(qrCodeImage)
+
+        document.close()
+        Toast.makeText(this,"Pdf Created", Toast.LENGTH_LONG).show()
+    }
 }
--- app/src/main/java/com/example/pbpkel4_tubes/MainActivity.kt
+++ app/src/main/java/com/example/pbpkel4_tubes/MainActivity.kt
@@ -16,8 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding =
-            ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnLogin.setOnClickListener {
 //            saveData()
